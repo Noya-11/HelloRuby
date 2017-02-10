a471ae0a835e8c91b8d9329505c682908bf98a1a
Noya-11 <c0114286f6@edu.teu.ac.jp>
2017-02-10 15:26:11 +0900

    test

--- hello.rb
+++ hello.rb
@@ -2,8 +2,22 @@ print "hello world"
 puts "hello world" #+改行
 p "hello world" #デバッグ用
 =begin
-	
+↑ここがコメントアウトの始まり	
 rescue Exception => e
 コメント文がここで書ける
+
+↓ここでコメントアウト終わりにする
 =end
 
+#変数について
+
+msg = "hello apple"
+puts msg
+
+msg = "hello apple again "
+puts msg
+
+#定数
+#-英大文字-
+VERSION = 1.1
+puts VERSION
